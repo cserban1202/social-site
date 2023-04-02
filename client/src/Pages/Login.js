341a561ba6f09deba8f50737f4c1264f34be8447
cserban1202 <cristiserban12@yahoo.com>
2023-04-02 17:03:25 +0300

    modules included

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -4,33 +4,31 @@ import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../helpers/AuthContext';
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState(""); 
-    const { setAuthState } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAuthState } = useContext(AuthContext);
 
-    let history = useNavigate();
-    
-    const login = () => {
-      const data = { username: username, password: password };
-      axios.post("http://localhost:3001/auth/login", data).then((response) => {
-        if (response.data.error) {
-          alert(response.data.error);
-        } else {
-          localStorage.setItem("accessToken", response.data.token);
-          setAuthState({username: response.data.username, id: response.data.id, status: true});
-          history("/");
-        }
-      });
-    };
+  let history = useNavigate();
+
+  const login = () => {
+    const data = { username: username, password: password };
+    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+      if (response.data.error) {
+        alert(response.data.error);
+      } else {
+        localStorage.setItem("accessToken", response.data.token);
+        setAuthState({ username: response.data.username, id: response.data.id, status: true });
+        history("/");
+      }
+    });
+  };
 
   return (
     <div className='loginContainer'>
-      <input type="text" placeholder="username" onChange={(event) =>
-        {setUsername(event.target.value)}}/>
-        <input type="password" placeholder="password" 
-        onChange={(event) =>
-        {setPassword(event.target.value)}}/>
-        <button onClick={login}>Login</button>
+      <input type="text" placeholder="username" onChange={(event) => { setUsername(event.target.value) }} />
+      <input type="password" placeholder="password"
+        onChange={(event) => { setPassword(event.target.value) }} />
+      <button onClick={login}>Login</button>
     </div>
   )
 }
--- client/src/Pages/Registration.js
+++ client/src/Pages/Registration.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -8,47 +8,47 @@ import { useNavigate } from 'react-router-dom';
 function Registration() {
 
   const navigate = useNavigate();
-    const initialValues = {
-        username: "",
-        password: "",
-    };
+  const initialValues = {
+    username: "",
+    password: "",
+  };
 
-    const validationSchema = Yup.object().shape({
-        username: Yup.string().min(3).max(15).required(),
-        password: Yup.string().min(4).max(20).required(),
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(3).max(15).required(),
+    password: Yup.string().min(4).max(20).required(),
+  });
+
+  //should make the api request, which then sends the data
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/auth", data).then((response) => {
+      console.log(response);
+      navigate("/login");
     });
+  }
 
-    //should make the api request, which then sends the data
-    const onSubmit = (data) => {
-        axios.post("http://localhost:3001/auth", data).then((response) => {
-          console.log(response);
-          navigate("/login");
-        });
-    }    
-    
   return (
     <div>
-      <Formik initialValues={initialValues} onSubmit ={onSubmit} validationSchema={validationSchema}>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
         <Form className='formContainer'>
-            <label>Username: </label>
-            <ErrorMessage name="username" component="span"/>
-            <Field 
+          <label>Username: </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
             autocomplete="off"
-            id="inputCreatePost" 
-            name="username" 
-            placeholder="Ex: John123.."/>
+            id="inputCreatePost"
+            name="username"
+            placeholder="Ex: John123.." />
 
-            <label>password: </label>
-            <ErrorMessage name="password" component="span"/>
-            <Field 
+          <label>password: </label>
+          <ErrorMessage name="password" component="span" />
+          <Field
             autocomplete="off"
             type="password"
-            id="inputCreatePost" 
-            name="password" 
-            placeholder="your password..."/>
+            id="inputCreatePost"
+            name="password"
+            placeholder="your password..." />
 
 
-            <button type="submit">Register</button>
+          <button type="submit">Register</button>
         </Form>
       </Formik>
     </div>
